9eb7c6329e34a937c20f5453693a2450d703e827
OhNooooooooo <109457389+OhNooooooooo@users.noreply.github.com>
2023-07-19 19:11:49 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+fifth = "";
+moon = "";
+
+function preload(){
+    fifth = loadSound("beethoven.mp3");
+    moon = loadSound("moon.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
